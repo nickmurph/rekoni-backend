88183cded09ddbea482dcebd1dd0b93b7d85a868
nickmurph <nfmurphy@buffalo.edu>
2023-04-12 01:10:37 -0600

    profile route implemented

--- server.js
+++ server.js
@@ -56,6 +56,19 @@ app.post('/register', (req,res) => {
     res.json(database.users[database.users.length-1])
 })
 
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if (!found){
+        res.status(400).json("no such user");
+    }
+})
 
 
 
