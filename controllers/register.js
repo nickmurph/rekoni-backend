329c48d82daeeb7d5ec57684eb11448d952c8207
nickmurph <nfmurphy@buffalo.edu>
2023-04-18 00:38:26 -0600

    added validation to register and signin controllers

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,10 @@ const handleRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
 
+    if (!email || !name || !password){
+        return res.status(400).json('incorrect form submission')
+    }
+
     db.transaction(trx => {
         trx.insert({
             hash: hash,
